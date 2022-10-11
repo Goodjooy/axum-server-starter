aec7fc04bed7b820dd898c0e7713e318fca4bbb3
Frozen String <964413011@qq.com>
2022-10-11 20:10:47 +0800

    ✨ adding derive into crate

--- codegen/derive_config_provider/src/lib.rs
+++ codegen/derive_config_provider/src/lib.rs
@@ -1,4 +1,4 @@
-#[proc_macro_derive(Provider, attributes(provide))]
-pub fn derive_config_provider(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(Provider, attributes(provider))]
+pub fn derive_config_provider(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     todo!()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,5 @@ pub use server_ready::ServerReady;
 
 pub use config_provide::{from_config::FromConfig, provider::Provider};
 pub use prepare_handler::{fn_prepare, FnPrepare, PrepareHandler};
+
+pub use derive_config_provider::Provider;
