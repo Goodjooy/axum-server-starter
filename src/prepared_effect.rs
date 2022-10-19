a502bd4d9193cba00be0e6ae3e33ff452cbbbf01
Frozen String <964413011@qq.com>
2022-10-19 09:06:22 +0800

    ✨ adding `EffectCollect`

--- src/prepared_effect.rs
+++ src/prepared_effect.rs
@@ -37,6 +37,161 @@ impl<T: PreparedEffect> IntoFallibleEffect for T {
     }
 }
 
+pub struct EffectsCollect<Route = (), Graceful = (), Extension = (), Server = ()>
+where
+    Route: RouteEffect,
+    Graceful: GracefulEffect,
+    Extension: ExtensionEffect,
+    Server: ServerEffect,
+{
+    route: Route,
+    graceful: Graceful,
+    extension: Extension,
+    server: Server,
+}
+
+impl<Route, Graceful, Extension, Server> PreparedEffect
+    for EffectsCollect<Route, Graceful, Extension, Server>
+where
+    Route: RouteEffect,
+    Graceful: GracefulEffect,
+    Extension: ExtensionEffect,
+    Server: ServerEffect,
+{
+    type Extension = Extension;
+
+    type Graceful = Graceful;
+
+    type Route = Route;
+
+    type Server = Server;
+
+    fn split_effect(self) -> (Self::Extension, Self::Route, Self::Graceful, Self::Server) {
+        let Self {
+            route,
+            graceful,
+            extension,
+            server,
+        } = self;
+        (extension, route, graceful, server)
+    }
+}
+
+impl<Route, Graceful, Extension, Server> EffectsCollect<Route, Graceful, Extension, Server>
+where
+    Route: RouteEffect,
+    Graceful: GracefulEffect,
+    Extension: ExtensionEffect,
+    Server: ServerEffect,
+{
+    pub fn with_route<R: RouteEffect>(
+        self,
+        new_route: R,
+    ) -> EffectsCollect<(Route, R), Graceful, Extension, Server> {
+        let Self {
+            route,
+            graceful,
+            extension,
+            server,
+        } = self;
+        EffectsCollect {
+            route: (route, new_route),
+            graceful,
+            extension,
+            server,
+        }
+    }
+
+    pub fn with_extension<E: ExtensionEffect>(
+        self,
+        new_extension: E,
+    ) -> EffectsCollect<Route, Graceful, (Extension, E), Server> {
+        let Self {
+            route,
+            graceful,
+            extension,
+            server,
+        } = self;
+        EffectsCollect {
+            route,
+            graceful,
+            extension: (extension, new_extension),
+            server,
+        }
+    }
+
+    pub fn with_server<S: ServerEffect>(
+        self,
+        new_server: S,
+    ) -> EffectsCollect<Route, Graceful, Extension, (Server, S)> {
+        let Self {
+            route,
+            graceful,
+            extension,
+            server,
+        } = self;
+        EffectsCollect {
+            route,
+            graceful,
+            extension,
+            server: (server, new_server),
+        }
+    }
+    pub fn with_graceful<G: GracefulEffect>(
+        self,
+        new_graceful: G,
+    ) -> EffectsCollect<Route, (Graceful, G), Extension, Server> {
+        let Self {
+            route,
+            graceful,
+            extension,
+            server,
+        } = self;
+        EffectsCollect {
+            route,
+            graceful: (graceful, new_graceful),
+            extension,
+            server,
+        }
+    }
+
+    pub fn with_effect<E: PreparedEffect>(
+        self,
+        effect: E,
+    ) -> EffectsCollect<
+        (Route, E::Route),
+        (Graceful, E::Graceful),
+        (Extension, E::Extension),
+        (Server, E::Server),
+    > {
+        let Self {
+            route,
+            graceful,
+            extension,
+            server,
+        } = self;
+        let effect = effect.split_effect();
+
+        EffectsCollect {
+            route: (route, effect.1),
+            graceful: (graceful, effect.2),
+            extension: (extension, effect.0),
+            server: (server, effect.3),
+        }
+    }
+}
+
+impl EffectsCollect {
+    pub fn new() -> Self {
+        Self {
+            route: (),
+            graceful: (),
+            extension: (),
+            server: (),
+        }
+    }
+}
+
 pub fn extension_only<S: PreparedEffect<Graceful = (), Route = (), Server = ()>>(
     extension: S::Extension,
 ) -> (S::Extension, S::Route, S::Graceful, S::Server) {
