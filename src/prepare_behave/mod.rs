5e7021bda63b294b3bbf173014354b4248852dd9
Frozen String <964413011@qq.com>
2022-12-03 18:47:30 +0800

    feat(re-export) re export element in effect_behavior

--- src/prepare_behave/mod.rs
+++ src/prepare_behave/mod.rs
@@ -1,2 +1,13 @@
 pub mod effect_collectors;
-pub mod traits;
+pub mod effect_contain;
+mod traits;
+
+pub mod effect_traits {
+    pub use super::traits::prepare_middleware::MiddlewarePrepareEffect;
+    pub use super::traits::prepare_route::PrepareRouteEffect;
+    pub use super::traits::prepare_state::PrepareStateEffect;
+    pub use super::traits::Prepare;
+}
+
+pub use effect_collectors::state_collector::{FromStateCollector, StateCollector};
+pub use effect_contain::EffectContainer;
